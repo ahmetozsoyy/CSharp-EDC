d643cd98bb6e8aefe8af213a66c7fe99a5fc1fac
Ahmet <ozsoyyahmett@gmail.com>
2025-07-30 00:26:46 +0300

    Entity Framework classes were created for the tables.

---
+++ CSharpEgitim.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -0,0 +1,15 @@
+﻿using CSharpEgitim.DataAccessLayer.Abstract;
+using CSharpEgitim.DataAccessLayer.Repositories;
+using CSharpEgitim.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitim.DataAccessLayer.EntityFramework
+{
+    public class EfProductDal: GenericRepository<Product>, IProductDal     
+    {
+    }
+}
